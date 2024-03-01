ef05dd823a0613804a199f53694632a891a4c8a4
BeleRohit <rohitnbele@gmail.com>
2024-03-01 15:53:10 +0530

    DataTansformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 
 @dataclass
 class DataIngestionConfig:
@@ -41,5 +44,8 @@ class DataIngestion:
             raise CustomException (e, sys)
 
 if __name__ == "__main__":
-    injestion=DataIngestion()
-    injestion.initiate_data_ingestion()
+    obj = DataIngestion ()
+    train_data,test_data,_ = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation ()
+    data_transformation.initiate_data_transformation(train_data,test_data)
